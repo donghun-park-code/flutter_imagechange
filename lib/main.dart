d78252efe022fa5b09c941003be1517ea040866d
unknown <sopu55544@gmail.com>
2022-11-16 22:07:50 +0900

    ㅇㅇ

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:imageproject/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: homescreen(),
+    )
+  );
+}
+
